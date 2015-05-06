f23fd0661609a10500b935028e4d7741608094ca
Alex <alexandrfromminsk@gmail.com>
2015-05-07 01:54:45 +0300

    Working on interface. Crashed on swipe. Mazafaka

--- app/src/main/java/com/by/alex/depositcalcupd/OnTabChangedListener.java
+++ app/src/main/java/com/by/alex/depositcalcupd/OnTabChangedListener.java
@@ -1,7 +1,7 @@
 package com.by.alex.depositcalcupd;
 
 public interface OnTabChangedListener {
-    public void saveFirstTabData(int position);
+    public void saveFirstTabData(String spn_currency, int summ, int timeperiod, int spn_tpr, String dateBegin, String dateEnd, float profit);
     public void saveSecondTabData(int position);
     public void saveCompareFragmentTabData(int position);
     public void getDataForSecondTab(int position);
