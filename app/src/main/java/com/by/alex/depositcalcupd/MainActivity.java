90863f3875f8c6c1f30984b1774aab33e8d6b976
Alex <alexandrfromminsk@gmail.com>
2015-05-07 02:14:56 +0300

    Working on interface. Crashed on swipe. Mazafaka

--- app/src/main/java/com/by/alex/depositcalcupd/MainActivity.java
+++ app/src/main/java/com/by/alex/depositcalcupd/MainActivity.java
@@ -137,7 +137,7 @@ public class MainActivity extends ActionBarActivity
         Log.e("", tab.getPosition() + " tab unselected");
         switch (tab.getPosition()){
             case TAB_ONE:
-                firstTab = (CurrencyOneFragment) mAdapter.ge;
+                firstTab = (CurrencyOneFragment) mAdapter.do_somthing;
                 //http://stackoverflow.com/questions/6976027/reusing-fragments-in-a-fragmentpageradapter
                 firstTab.saveData();
 
