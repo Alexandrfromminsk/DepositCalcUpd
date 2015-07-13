18fba3bdd694ad9cb31137268e0f67cfea0e45b2
Aliaksandr Baltruk <aliaksandr_baltruk@epam.com>
2015-07-13 19:20:28 +0300

    PreferenceActivity use a custom preference file

--- app/src/main/java/com/by/alex/depositcalcupd/SettingsActivity.java
+++ app/src/main/java/com/by/alex/depositcalcupd/SettingsActivity.java
@@ -2,12 +2,18 @@ package com.by.alex.depositcalcupd;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PreferenceManager prefMgr = getPreferenceManager();
+        prefMgr.setSharedPreferencesName(MainActivity.APP_PREFERENCES);
+        prefMgr.setSharedPreferencesMode(MODE_PRIVATE);
+
         addPreferencesFromResource(R.xml.prefrences);
     }
 }
