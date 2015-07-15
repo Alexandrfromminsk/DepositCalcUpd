51bcf16ba57c598e5806c4241449459512775714
Alex <alexandrfromminsk@gmail.com>
2015-07-15 17:22:14 +0300

    Prepare inverset dependancy in settings for new tax

--- app/src/main/java/com/by/alex/depositcalcupd/SettingsActivity.java
+++ app/src/main/java/com/by/alex/depositcalcupd/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.by.alex.depositcalcupd;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    CheckBoxPreference ut, rt;
+    SharedPreferences mSettings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,74 @@ public class SettingsActivity extends PreferenceActivity {
         prefMgr.setSharedPreferencesMode(MODE_PRIVATE);
 
         addPreferencesFromResource(R.xml.prefrences);
+
+        mSettings = getSharedPreferences(MainActivity.APP_PREFERENCES,MODE_PRIVATE);
+        mSettings.registerOnSharedPreferenceChangeListener(this);
+
+        ut = (CheckBoxPreference)findPreference("ukr_tax");
+        rt = (CheckBoxPreference)findPreference("russian_tax");
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        switch (key){
+            case "russian_tax":
+                if (rt.isChecked()) {
+                    ut.setChecked(false);
+
+                    SharedPreferences.Editor ed = mSettings.edit();
+                    ed.putBoolean("ukr_tax", false); // your updated setting
+                    ed.commit();
+                }
+                break;
+            case "ukr_tax":
+                if (ut.isChecked()) {
+                    rt.setChecked(false);
+
+                    SharedPreferences.Editor edt = mSettings.edit();
+                    edt.putBoolean("russian_tax", false); // your updated setting
+                    edt.commit();
+                }
+                break;
+        }
+/*
+        if (key.equals("russian_tax"))
+        {
+            ut.setChecked(false);
+
+            SharedPreferences.Editor ed = mSettings.edit();
+            ed.putBoolean("ukr_tax", false); // your updated setting
+            ed.commit();
+            //Preference preference = findPreference("ukr_tax");
+            //preference.setEnabled(false);
+        }
+
+        else if (key.equals("ukr_tax"))
+        {
+            rt.setChecked(false);
+
+            SharedPreferences.Editor ed = mSettings.edit();
+            ed.putBoolean("russian_tax", false); // your updated setting
+            ed.commit();
+            //Preference preference = findPreference("russian_tax");
+            //preference.setEnabled(false);
+        }
+*/
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+// Set up a listener whenever a key changes
+        mSettings.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+// Unregister the listener whenever a key changes
+        mSettings.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
